c0ff572791e9e091fa7dc79ad7700a4d877f3a33
johnacs <johnacsyen@gmail.com>
2017-01-10 17:44:06 +0800

    created createCards function

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
-var cardTwo = "king";
+var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+/*
+if (cardTwo === cardFour){
+	alert("You found a match!")
+} else{
+	alert("Sorry, try again.")
+}
+*/
+var createCards = function(){
+	var gameBoard = document.getElementById("game-board");
+
+for(i=0; i<4; i++){
+	var myDiv = document.createElement('div');
+	gameBoard.appendChild(myDiv);
+	myDiv.className = 'card';
+}
+}
+
+createCards();
